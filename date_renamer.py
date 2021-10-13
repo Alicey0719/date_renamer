15f7c7dc7bf70a4c9575ebbb8618b45f9a834afc
Alicey <sirius@alicey.dev>
2021-10-14 01:01:06 +0900

    [update] 最低限動作する状態

--- date_renamer.py
+++ date_renamer.py
@@ -1,16 +1,38 @@
 from pathlib import Path
 import datetime
+import shutil
+import glob
+import sys
+import os
 
 def file_move(from_file_path, to_dir_path):
-    return
+    shutil.move(from_file_path, to_dir_path)
 
 def get_filedate(path):
     path = Path(path)
     time = datetime.date.fromtimestamp(path.stat().st_mtime)
     return time
 
+def get_filelist(dir_path):
+    l = glob.glob(dir_path+'[!*.py]')
+    # print(l)
+    return l
+
 def main():
-    a = get_filedate(r'test.jpg')
-    print(type(a),a )
+    ## pathの取得
+    try:
+        filelist = get_filelist(sys.argv[1])
+    except IndexError:
+        print('ERROR:引数に対象を指定してください.')
+
+    ## 移動処理
+    for f in filelist:
+        fdate = get_filedate(f)
+        try:
+            os.mkdir(str(fdate))
+        except FileExistsError:
+            pass
+        file_move(f, str(fdate)+'/'+f)
+
 
 if __name__ == '__main__': main()
